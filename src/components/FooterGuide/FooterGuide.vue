3c081b7e9ebac7efb05f22d8cbc70e926e20332d
MrTseng <zyckg21@qq.com>
2019-02-12 09:33:53 +0800

    项目首次移植到git版本控制

---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,73 @@
+<template>
+    <footer class="footer_guide">
+    <a href="javascript:;" class="guide_item" :class="{on:'/msite'===$route.path}" @click="goTo('/msite')"> <!-- 判断当前是否有on，（在vue里a就是路由）当路由的请求路径等于当前的/msite为true，添加上on -->
+      <span class="item_icon">
+        <i class="iconfont icon-waimai"></i>
+      </span>
+      <span>外卖</span>
+    </a>
+    <a href="javascript:;" class="guide_item" :class="{on:'/search'===$route.path}" @click="goTo('/search')">
+      <span class="item_icon">
+        <i class="iconfont icon-search"></i>
+      </span>
+      <span>搜索</span>
+    </a>
+    <a href="javascript:;" class="guide_item" :class="{on:'/order'===$route.path}" @click="goTo('/order')">
+      <span class="item_icon">
+        <i class="iconfont icon-dingdan"></i>
+      </span>
+      <span>订单</span>
+    </a>
+    <a href="javascript:;" class="guide_item" :class="{on:'/profile'===$route.path}" @click="goTo('/profile')">
+      <span class="item_icon">
+        <i class="iconfont icon-geren"></i>
+      </span>
+      <span>我的</span>
+    </a>
+  </footer>
+</template>
+
+<script>
+  export default {
+    methods:{
+      //导航通过绑定点击事件@click来跳转路径
+      //$route为当前router跳转对象里面可以获取name、path、query、params等
+      //$router为VueRouter实例，想要导航到不同URL，则使用$router.push方法
+      //返回上一个history也是使用$router.go方法
+      goTo(path){
+        this.$router.replace(path)
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixins.styl"
+  .footer_guide  //footer
+    top-border-1px(#e4e4e4)
+    position fixed
+    z-index 100
+    left 0
+    right 0
+    bottom 0
+    background-color #fff
+    width 100%
+    height 50px
+    display flex
+    .guide_item
+      display flex
+      flex 1
+      text-align center
+      flex-direction column
+      align-items center
+      margin 5px
+      color #999999
+      &.on
+        color #02a774
+      span
+        font-size 12px
+        margin-top 2px
+        margin-bottom 2px
+        .iconfont
+          font-size 22px
+</style>
---
+++ src/pages/Msite/Msite.vue
@@ -0,0 +1,24 @@
+<template>
+  <HeaderTop title="昌平区北七家宏福科技园(337省道北)">
+    <span class="header_search" slot="left">
+      <i class="iconfont icon-sousuo"></i>
+    </span>
+    <span class="header_login" slot="right">
+      <span class="header_login_text">登录|注册</span>
+    </span>
+  </HeaderTop>
+</template>
+
+<script>
+//组件的使用步骤1，引入组件路径
+import HeaderTop from '../../components/HeaderTop/HeaderTop.vue';
+//组件的使用步骤2，映射成标签
+export default {
+    components: {
+        HeaderTop
+    }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
